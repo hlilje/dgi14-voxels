a652a2cc09133a3317c704492ea816cbc04789b3
Hampus Liljekvist <hlilje@users.noreply.github.com>
2014-04-28 11:53:47 +0200

    Work on project setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <GL/glut.h>
+#include <glm/glm.hpp>
+
+void draw();
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+    glutInit(&argc, argv);
+
+    // Simple buffer
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(50, 25);
+    glutInitWindowSize(500, 250);
+    glutCreateWindow("Green Window");
+
+    // Draw
+    glutDisplayFunc(draw);
+    glutMainLoop();
     cout << "Hello" << endl;
     return 0;
+}
+
+void draw(void)
+{
+  // Background colour
+  glClearColor(0, 1, 0, 1);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  // Draw order
+  glFlush();
 }
